ff7a41b81ecf20639ff65373e31783b99d89dc67
tmtabor <tmtabor@cloud.ucsd.edu>
2024-07-19 09:18:47 -0700

    Fixed bug in creating mount targets in the right subnets, added debugging lines

--- kubernetes/setup-cluster.sh
+++ kubernetes/setup-cluster.sh
@@ -51,6 +51,7 @@ done
 # Create the cluster                                       #
 ############################################################
 
+echo "CREATING THE CLUSTER"
 eksctl create cluster --name $cluster_name --region $aws_region
 
 ############################################################
@@ -74,51 +75,49 @@ printf '\n%s\n' "Cluster creation completed successfully after $count attempts."
 ############################################################
 
 # Set the IAM OIDC provider associated with cluster
+echo "SETTING UP THE IAM OIDC PROVIDER"
 eksctl utils associate-iam-oidc-provider --region=$aws_region --cluster=$cluster_name --approve
 
 # Create the IAM role
+echo "CREATING THE IAM ROLE"
 eksctl create iamserviceaccount --name efs-csi-controller-sa  --namespace kube-system --cluster $cluster_name --role-name $role_name --region $aws_region --role-only --attach-policy-arn arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy --approve
 TRUST_POLICY=$(aws iam get-role --role-name $role_name --query 'Role.AssumeRolePolicyDocument' | sed -e 's/efs-csi-controller-sa/efs-csi-*/' -e 's/StringEquals/StringLike/')
 aws iam update-assume-role-policy --role-name $role_name --policy-document "$TRUST_POLICY"
 
 # Attach the EFS driver to the cluster
+echo "ATTACHING THE EFS DRIVER TO THE CLUSTER"
 ROLE_ARN=$(aws iam get-role --role-name $role_name --query 'Role.[RoleName, Arn]' --output text | awk '{print $2}')
 eksctl create addon --cluster $cluster_name --name aws-efs-csi-driver --version latest --service-account-role-arn $ROLE_ARN --region $aws_region
 
 # Create the EFS file system for use by the cluster
+echo "CREATING THE EFS FILE SYSTEM"
 vpc_id=$(aws eks describe-cluster --name $cluster_name --region $aws_region --query "cluster.resourcesVpcConfig.vpcId" --output text)
 cidr_range=$(aws ec2 describe-vpcs --vpc-ids $vpc_id --query "Vpcs[].CidrBlock" --output text --region $aws_region)
 security_group_id=$(aws ec2 create-security-group --group-name $security_group --description "Access EFS drive from EKS" --vpc-id $vpc_id --region $aws_region --output text)
 aws ec2 authorize-security-group-ingress --group-id $security_group_id --protocol tcp --port 2049 --cidr $cidr_range --region $aws_region
 file_system_id=$(aws efs create-file-system --region $aws_region --performance-mode generalPurpose --query 'FileSystemId' --output text)
-aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" --query 'Subnets[*].{SubnetId: SubnetId,AvailabilityZone: AvailabilityZone,CidrBlock: CidrBlock}' --region $aws_region --output table | tail -n +6 | awk '{print $6}' | xargs -Isubnet_id aws efs create-mount-target --file-system-id $file_system_id --security-groups $security_group_id --region $aws_region --subnet-id subnet_id
+aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" --query 'Subnets[*].{SubnetId: SubnetId,AvailabilityZone: AvailabilityZone,CidrBlock: CidrBlock}' --region $aws_region --output text | awk '{print $3}' | xargs -Isubnet_id aws efs create-mount-target --file-system-id $file_system_id --security-groups $security_group_id --region $aws_region --subnet-id subnet_id
 
 # Create the EFS storage class
-# echo "{\"apiVersion\": \"storage.k8s.io/v1\", \"kind\": \"StorageClass\", \"provisioner\": \"efs.csi.aws.com\", \"metadata\": {\"name\": \"efs-sc\", \"annotations\": {\"storageclass.kubernetes.io/is-default-class\": \"true\"}}, \"parameters\": {\"server\": \"${file_system_id}.efs.us-west-2.amazonaws.com\", \"path\": \"/\", \"readOnly\": \"false\"}}" | kubectl apply -f -
+echo "CREATING THE EFS STORAGE CLASS"
 echo "{ \"apiVersion\": \"storage.k8s.io/v1\", \"kind\": \"StorageClass\", \"metadata\": { \"name\": \"efs-sc\", \"annotations\": { \"storageclass.kubernetes.io/is-default-class\": \"true\" } }, \"provisioner\": \"efs.csi.aws.com\", \"parameters\": { \"provisioningMode\": \"efs-ap\", \"fileSystemId\": \"${file_system_id}\", \"directoryPerms\": \"700\", \"gidRangeStart\": \"1000\", \"gidRangeEnd\": \"2000\", \"basePath\": \"/dynamic_provisioning\", \"subPathPattern\": \"\${.PVC.namespace}/\${.PVC.name}\", \"ensureUniqueDirectory\": \"true\", \"reuseAccessPoint\": \"false\" }}" | kubectl apply -f -
-
-#kubectl apply -f efs-sc.yaml
-#kubectl apply -f efs.yaml
-
-# kubectl create namespace notebook-workspace
-#> kubectl --namespace=g2nb-workspace apply -f test_efs.yaml
-#> kubectl --namespace=g2nb-workspace apply -f test_efs_claim.yaml
-
 
 ############################################################
 # Deploy JupyterHub                                        #
 ############################################################
 
 # Load the JupyterHub helm chart
+echo "LOADING THE JUPYTERHUB HELM CHART"
 helm repo add jupyterhub https://hub.jupyter.org/helm-chart/
 helm repo update
 
 # Deploy JupyterHub to the cluster
+echo "DEPLOYING THE JUPYTERHUB APPLICATION TO THE CLUSTER"
 helm upgrade --cleanup-on-fail --install $namespace jupyterhub/jupyterhub --namespace $namespace --create-namespace --version=3.3.7 --values jupyterhub-config.yaml
 
 # Return the public URL of the hub, run:
 sleep 10
-echo "Wait for all pods to start up and then visit the following URL"
+echo "Wait for all pods to start up and then visit the following URL... It may take a few minutes."
 echo http://$(kubectl --namespace $namespace get service proxy-public --output jsonpath='{.status.loadBalancer.ingress[].hostname}')
 
 # You can make sure all the necessary pods are created by running the following:
